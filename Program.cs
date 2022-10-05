2f32b20c1a04c08885fae7754fa71c5a5c939426
OlegSs <youremail@yourdomain.comolegs934@gmail.com>
2022-10-05 16:36:22 +0300

    Задача решена

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число : ");
+int n=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Четные числа от 1 до " + n);
+int i = 1;
+while (i <= n)
+{
+    if (i % 2 != 1)
+    {
+        Console.Write(i + ", ");
+    }
+    i++;
+}
+    
